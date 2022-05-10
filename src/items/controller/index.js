caaed18e09e0fd8dbb04ace1ed19d84c8ba540b2
Bassel Ashi <passel.y@el-ashi.net>
2022-05-10 17:18:34 -0400

    Add item creation logic

--- src/items/controller/index.js
+++ src/items/controller/index.js
@@ -3,10 +3,24 @@ const schemes = require('../models/mongoose');
 
 module.exports.createItem = async (res, parameters) => {
   const {
-    id,
     name,
     price
   } = parameters;
   
-  return res.send("Item creation here");
+  const newItem = schemes.Item({
+    name: parameters.name,
+    price: parameters.price,
+  });
+
+  try {
+    const savedItem = await newItem.save();
+    return res.status(201).json(savedItem);
+
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({
+      status: 400,
+      message: error,
+    });
+  }
 };
